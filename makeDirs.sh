957c4d59588bb09a615ecb95d9ba98f3d92ded0a
Mark Mikkelsen <mark.mikkelsen@gmail.com>
2024-06-12 17:21:20 -0400

    Update makeDirs.sh
    - Make a study directory that all subdirectories will be created in
    - Add `-p` to `mkdir` commands so no errors are displayed if the parent directories already exist

--- makeDirs.sh
+++ makeDirs.sh
@@ -4,16 +4,24 @@
 #structure is loosely based on BIDS MRS standard, but without 
 #the requirement of storing the data in nifti-MRS format.  
 #Jamie Near, Sunnybrook Research Institute, 2024
+#Updates by Mark Mikkelsen, Weill Cornell Medicine, 2024
 
+#First make the study directory
+studydir='DPXX'
+if [ ! -d $studydir ]
+then
+	mkdir -p $studydir
+fi
+echo Study directory: $studydir
 
-#First make an array of Subject IDs (N=8):
+#Then make an array of Subject IDs (N=8):
 subs=(01 02 03 04 05 06 07 08)
-echo $subs
+echo Subject IDs: $subs
 
 #Now make an array of Session IDs:
 ###(remove 02 if only one timepoint)
 sessions=(01 02)
-echo $sessions
+echo Session IDs: $sessions
 
 #initialize the value of the mrs 'acq' field:
 ###(edit 'press' to your mrs sequence of choice)
@@ -33,20 +41,18 @@ anatacq='3drare'
 ###(axial ('ax'), sagittal('sag'), or coronal ('cor'))
 anatorient='cor'
 
-
-#Loop through all subjects and session to make all needed directories:
+#Loop through all subjects and sessions to make all needed directories:
 for n in $subs; do
-	mkdir sub-${n} #make subject directory first
-	
+	mkdir -p $studydir/sub-${n} #make subject directory first	
 	for m in $sessions; do
 		#make session directories and subdirectories:
-		mkdir sub-${n}/ses-${m}
-		mkdir sub-${n}/ses-${m}/mrs
-		mkdir sub-${n}/ses-${m}/anat
-		mkdir sub-${n}/ses-${m}/other
-		#mkdir sub-${n}/ses-${m}/mrs/sub-${n}_ses-${m}_acq-${mrsacq}_voi-${mrsvoi}_svs
-		#mkdir sub-${n}/ses-${m}/mrs/sub-${n}_ses-${m}_acq-${mrsacq}_voi-${mrsvoi}_mrsref
-		#mkdir sub-${n}/ses-${m}/anat/sub-${n}_ses-${m}_acq-${anatacq}_anat-${anatorient}
+		mkdir -p $studydir/sub-${n}/ses-${m}
+		mkdir -p $studydir/sub-${n}/ses-${m}/mrs
+		mkdir -p $studydir/sub-${n}/ses-${m}/anat
+		mkdir -p $studydir/sub-${n}/ses-${m}/other
+		#mkdir -p $studydir/sub-${n}/ses-${m}/mrs/sub-${n}_ses-${m}_acq-${mrsacq}_voi-${mrsvoi}_svs
+		#mkdir -p $studydir/sub-${n}/ses-${m}/mrs/sub-${n}_ses-${m}_acq-${mrsacq}_voi-${mrsvoi}_mrsref
+		#mkdir -p $studydir/sub-${n}/ses-${m}/anat/sub-${n}_ses-${m}_acq-${anatacq}_anat-${anatorient}
 	done
 done
 
